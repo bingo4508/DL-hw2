ff411656e8c69152391c5198810fedfafd804280
alex1230608 <alex.eed99@nctu.edu.tw>
2015-05-06 22:27:32 +0800

    modify svm_struct_main.c and svm_struct_api_types.h for dummy and constant one (add one)

--- svm_struct/svm_struct_main.c
+++ svm_struct/svm_struct_main.c
@@ -189,6 +189,8 @@ void read_input_parameters(int argc,char *argv[],char *trainfile,
       case '-': strcpy(struct_parm->custom_argv[struct_parm->custom_argc++],argv[i]);i++; strcpy(struct_parm->custom_argv[struct_parm->custom_argc++],argv[i]);break; 
       case 'v': i++; (*struct_verbosity)=atol(argv[i]); break;
       case 'y': i++; (*verbosity)=atol(argv[i]); break;
+	  case '0': i++; struct_parm->dummy = atol(argv[i]); break;
+	  case '1': i++; struct_parm->addOne = atol(argv[i]); break;
       default: printf("\nUnrecognized option %s!\n\n",argv[i]);
 	       print_help();
 	       exit(0);
@@ -351,6 +353,8 @@ void print_help()
   printf("                        0: zero/one loss\n");
   printf("                        ?: see below in application specific options\n");
   printf("                        (default %d)\n",DEFAULT_LOSS_FCT);
+  printf("         -0          -> Use Dummy.\n");
+  printf("         -1          -> Use Constant one.\n");
   printf("Optimization Options (see [2][5]):\n");
   printf("         -w [0,..,9] -> choice of structural learning algorithm (default %d):\n",(int)DEFAULT_ALG_TYPE);
   printf("                        0: n-slack algorithm described in [2]\n");
